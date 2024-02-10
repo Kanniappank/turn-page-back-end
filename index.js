540fb7a9389e91d0f85a66aa764b8ed3b24f6024
Kanniappan <suriyastart007@gmail.com>
2024-02-11 04:06:36 +0530

    worknig of front end

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const app = express();
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
+
 
 // MySQL database connection configuration
 const dbConfig = {
@@ -31,11 +32,19 @@ app.post('/register', async (req, res) => {
     try {
       console.log('register method called');
       const { username, email, password } = req.body;
+      if(username=='' || email=='' || password==''){
+        return res.status(400).json({ error: 'Please Enter all the fields' });
+      }
 
       console.log(req.body);
   
       // Insert user into the database
       const connection = await pool.getConnection();
+      const [rows]=await connection.query('select * from users where Email= ? and password= ? ',[username,email])
+      connection.release();
+      if(rows.length >0){
+        return res.status(201).json({ message: 'User Already Exists',success:true });
+      }
       await connection.query('INSERT INTO users (user_name, Email, password) VALUES (?, ?, ?)', [username, email, password]);
       connection.release();
   
@@ -64,18 +73,48 @@ app.post('/register', async (req, res) => {
 
         // Check if user exists and password is correct
         if (rows.length === 0) {
-            return res.status(401).json({ error: 'Invalid email or password' });
+            return res.status(200).json({ success:false,message: 'Invalid email or password' });
         }
 
-        res.status(200).json({ message: 'User logged in successfully', success: true ,userId:rows[0].id});
+        res.status(200).json({ success: true ,userId:rows[0].id ,message: 'User logged in successfully', });
     } catch (error) {
         console.error('Error logging in:', error);
         res.status(500).json({ error: 'Error logging in' });
     }
 });
+app.post('/feedback', async (req, res) => {
+  try {
+      const { feedback,user_id } = req.body;
+
+
+      // Check if email and password are provided
+      if (!feedback || !user_id) {
+          return res.status(400).json({ error: 'Feedback and userId are required' });
+      }
+
+      // Query the database to find the user by email and password
+      const connection = await pool.getConnection();
+      await connection.query('update users set user_feedback=? where id=?', [feedback, user_id]);
+      connection.release();
+
+      res.status(200).json({ success:true,message: 'Thanks for your valuble feedback' });
+
+
+      // console.log('rows',rows);
+
+      // Check if user exists and password is correct
+      // if (rows.length === 0) {
+      //     return res.status(200).json({ success:false,message: 'Invalid email or password' });
+      // }
+
+  } catch (error) {
+      console.error('Error logging in:', error);
+      res.status(500).json({ error: 'Error logging in' });
+  }
+});
     
   
-  app.post('/books', async (req, res) => {
+  app.post('/add-book', async (req, res) => {
     try {
       const { title, author, user_id,book_link,book_description} = req.body;
   
@@ -90,6 +129,7 @@ app.post('/register', async (req, res) => {
       res.status(500).json({ error: 'Error adding book' });
     }
   });
+  
   
   app.delete('/books/:id', async (req, res) => {
     try {
@@ -107,14 +147,27 @@ app.post('/register', async (req, res) => {
     }
   });
   
-  app.get('/books', async (req, res) => {
+  app.get('/audio-books', async (req, res) => {
     try {
       // Fetch all books from the database
       const connection = await pool.getConnection();
-      const [rows, fields] = await connection.query('SELECT * FROM books');
+      const [rows, fields] = await connection.query('SELECT * FROM books where book_format=1');
       connection.release();
   
-      res.status(200).json({ books: rows });
+      res.status(200).json({ success:true,message:'Audio books retrived succsfully',books: rows });
+    } catch (error) {
+      console.error('Error getting books:', error);
+      res.status(500).json({ error: 'Error getting books' });
+    }
+  });
+  app.get('/text-books', async (req, res) => {
+    try {
+      // Fetch all books from the database
+      const connection = await pool.getConnection();
+      const [rows, fields] = await connection.query('SELECT * FROM books where book_format=0');
+      connection.release();
+  
+      res.status(200).json({ success:true,message:'Audio books retrived succsfully',books: rows });
     } catch (error) {
       console.error('Error getting books:', error);
       res.status(500).json({ error: 'Error getting books' });
@@ -143,7 +196,7 @@ app.post('/register', async (req, res) => {
       await connection.query('UPDATE books SET title = ?, author = ? WHERE id = ?', [title, author, bookId]);
       connection.release();
   
-      res.status(200).json({ message: 'Book updated successfully' });
+      // res.status(200).json({ success:true,message: 'Thanks for your valuble feedback' });
     } catch (error) {
       console.error('Error updating book:', error);
       res.status(500).json({ error: 'Error updating book' });
